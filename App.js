58b57c01c800825f70d05c44d82f7c3920182b5d
Carlos <carlos.schuenck.rodrigues@gmail.com>
2021-01-16 19:33:09 -0300

    Thatsmynoteapp

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [state , setState] = useState ('read');
+  const [note, setNote] = useState ('My note aaaaaaaaaaaaaaaaaaaa');
+
+  if(state == 'read'){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <StatusBar style="light"/>
+      
+      <View style={styles.header}><Text style ={{textAlign: 'center', color: 'white', fontSize: 25}}>That's My note</Text></View>
+
+      <View style= {{padding:30}}><Text style={styles.note}>{note}</Text></View>
+
+      <TouchableOpacity onPress={()=> setState('loading') } style={styles.btnNote}><Text style={styles.btnNoteTxt}>+</Text></TouchableOpacity>
     </View>
   );
+
+  }else if(state == "loading"){
+    return(
+      <View style={{ flex: 1 }}>
+        <StatusBar style="light" />
+
+        <View style={styles.header}><Text style={{ textAlign: 'center', color: 'white', fontSize: 25 }}>That's My note</Text></View>
+
+        <TouchableOpacity onPress={()=> setState('read')} style={styles.btnSave}><Text style={styles.btnSaveTxt}>Save</Text></TouchableOpacity>
+      </View>
+    );
+  }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    header:{
+      width:'100%',
+      padding:17,
+      backgroundColor: '#070'
+    },
+    note:{
+      fontSize: 20,
+    },
+    btnNote:{
+      position: 'absolute',
+      right: 40,
+      bottom:20,
+      width:50,
+      height: 50,
+      backgroundColor: '#070',
+      borderRadius:10,
+    },
+    btnNoteTxt:{
+      color:'white',
+      position: 'relative',
+      textAlign: 'center',
+      top:10,
+      fontSize:20,
+    },
+    btnSave:{
+      position: 'absolute',
+      right: 40,
+      bottom: 20,
+      width: 50,
+      height: 50,
+      backgroundColor: '#070',
+      borderRadius: 10,
+    },
+    btnSaveTxt:{
+      color: 'white',
+      position: 'relative',
+      textAlign: 'center',
+      top: 13,
+      fontSize: 15,
+    }
+
 });
